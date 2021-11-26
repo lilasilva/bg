956b4b4edc34192ec451ef51b379b474f9d7c112
lilasilva <94986224+lilasilva@users.noreply.github.com>
2021-11-26 18:01:43 -0300

    Update jason_list.js

--- xatspace-lila/jason_list.js
+++ xatspace-lila/jason_list.js
@@ -2,82 +2,82 @@
 var listModel = [
       {
             img: 'https://img.xatblog.net/image/SLVgnMLAd.png',
-            desc: 'Fundo - Sentimentos'
+            desc: 'Fundo  1'
       },
       {
             img: 'https://img.xatblog.net/image/dDGJJpOtL.png',
-            desc: 'Fundo - Rdiabrete'
+            desc: 'Fundo  2'
       },
       {
             img: 'https://img.xatblog.net/image/TwlG5ef128G.png',
-            desc: 'Fundo Estrelina '
+            desc: 'Fundo   3 '
       },
       {
             img: 'https://img.xatblog.net/image/ER9yanHHL7.png',
-            desc: 'Fundo Taz'
+            desc: 'Fundo 4'
       },
       {
             img: 'https://i.postimg.cc/YSPrZmGK/agp1.png',
-            desc: 'Fundo - 1'
+            desc: 'Fundo 5'
       },
       {
             img: 'https://i.postimg.cc/y8Zyk29w/aggg.png',
-            desc: 'Fundo - 2'
+            desc: 'Fundo 6'
       },
       {
             img: 'https://i.postimg.cc/Bn6G3nGq/MEUU.png',
-            desc: 'Fundo - 3'
+            desc: 'Fundo 7'
       },
       {
             img: 'https://i.postimg.cc/9Fm6PQ0G/lagrima-ardente-1.png',
-            desc: 'Fundo - 4'
+            desc: 'Fundo 8'
       },
       {
             img: 'https://i.postimg.cc/7PVcLr9b/lov.png',
-            desc: 'Fundo - 5'
+            desc: 'Fundo 9'
       },
       {
             img: 'https://i.postimg.cc/C1tRGQH8/teste.png',
-            desc: 'Fundo - 6'
+            desc: 'Fundo 10'
       },
       {
             img: 'https://i.postimg.cc/VsdZBZck/MEU11.png',
-            desc: 'Fundo - 7'
+            desc: 'Fundo 11'
       },
       {
             img: 'https://i.postimg.cc/9FypJvgC/cdcd.png',
-            desc: 'Fundo - 8'
+            desc: 'Fundo 12'
       },
       
       {
             img: 'https://i.postimg.cc/0Ny05rky/ddaa.png',
-            desc: 'Fundo - 9'
+            desc: 'Fundo 13'
       },
       
       
       {
             img: 'https://i.postimg.cc/N0nmg4Cd/fundo-pvt-la.png',
-            desc: 'Fundo - 10'
+            desc: 'Fundo 14'
       },
       {
             img: 'https://i.postimg.cc/26mmBpty/eu3.png',
-            desc: 'Fundo - 11'
+            desc: 'Fundo 15'
       },
       {
             img: 'https://i.postimg.cc/VvKY87D6/xat-png-11.png',
-            desc: 'Fundo - 12'
+            desc: 'Fundo 16'
       },
       
       {
             img: 'https://i.postimg.cc/zBftQ0bp/DIABRETE-NATAL.png',
-            desc: 'Fundo - 14'
+            desc: 'Fundo 17'
       },
       {
             img: 'https://i.postimg.cc/YCHwPBp3/radio-diabreta-f.png',
-            desc: 'Fundo - 15'
+            desc: 'Fundo 18'
       },
       {
             img: 'https://i.postimg.cc/T25MY310/faty-frentee.png',
-            desc: 'Fundo - 16'
+            desc: 'Fundo 19'
       },
 ]
