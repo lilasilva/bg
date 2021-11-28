196b26c1c554437270d0671e87ac673592cf0077
Lila Love <94986224+lilasilva@users.noreply.github.com>
2021-11-28 15:51:56 -0300

    Update jason_list.js

--- xatspace-lila/jason_list.js
+++ xatspace-lila/jason_list.js
@@ -17,80 +17,42 @@ var listModel = [
             desc: 'Fundo 4'
       },
       {
-            img: 'https://i.postimg.cc/YSPrZmGK/agp1.png',
+            img: 'https://img.xatblog.net/image/P4l8P6wY8.png',
             desc: 'Fundo 5'
       },
+      
       {
-            img: 'https://i.postimg.cc/y8Zyk29w/aggg.png',
+            img: 'https://img.xatblog.net/image/PzU8J7W7L.png',
             desc: 'Fundo 6'
       },
       {
-            img: 'https://i.postimg.cc/Bn6G3nGq/MEUU.png',
+            img: 'https://img.xatblog.net/image/ixcvTioY2qx.png',
             desc: 'Fundo 7'
-      },
-      {
-            img: 'https://i.postimg.cc/9Fm6PQ0G/lagrima-ardente-1.png',
-            desc: 'Fundo 8'
-      },
-      {
-            img: 'https://i.postimg.cc/7PVcLr9b/lov.png',
-            desc: 'Fundo 9'
-      },
-      {
-            img: 'https://i.postimg.cc/C1tRGQH8/teste.png',
-            desc: 'Fundo 10'
-      },
-      {
-            img: 'https://i.postimg.cc/VsdZBZck/MEU11.png',
-            desc: 'Fundo 11'
-      },
-      {
-            img: 'https://i.postimg.cc/9FypJvgC/cdcd.png',
-            desc: 'Fundo 12'
-      },
-      
-      {
-            img: 'https://i.postimg.cc/0Ny05rky/ddaa.png',
-            desc: 'Fundo 13'
-      },
-      
-      
-      {
-            img: 'https://i.postimg.cc/N0nmg4Cd/fundo-pvt-la.png',
-            desc: 'Fundo 14'
-      },
-      {
-            img: 'https://i.postimg.cc/26mmBpty/eu3.png',
-            desc: 'Fundo 15'
-      },
-      {
-            img: 'https://i.postimg.cc/VvKY87D6/xat-png-11.png',
-            desc: 'Fundo 16'
       },
       
      
       {
-            img: 'https://i.postimg.cc/k53MmYxJ/1TODPW6.png',
-            desc: 'Fundo 17'
+            img: 'https://img.xatblog.net/image/cehMNZf6yd.png',
+            desc: 'Fundo 8'
       },
       
       {
-            img: 'https://i.postimg.cc/wxGpzbpC/lila.png',
-            desc: 'Fundo 18'
+            img: 'https://img.xatblog.net/image/QHmKV6hRD.png',
+            desc: 'Fundo 9'
       },
       {
             img: 'https://img.xatblog.net/image/u98Gzhewfpg.png',
-            desc: 'Fundo 19'
+            desc: 'Fundo 10'
       },
       
       {
-            img: 'https://i.postimg.cc/t420rr2v/gue-f.png',
-            desc: 'Fundo 20'
+            img: 'https://img.xatblog.net/image/tXGVlvPwst.png',
+            desc: 'Fundo 11'
       },
       
       {
-            img: 'https://i.postimg.cc/5Ngk1XL6/5wgbzp.png',
-            desc: 'Fundo 21'
+            img: 'https://img.xatblog.net/image/kmJT13E9PY.png',
+            desc: 'Fundo 12'
       },
       
 ]
