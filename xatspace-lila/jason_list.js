3dd489a785db6c1c0b5f4ef927127373bb44d0c9
Lila Love <94986224+lilasilva@users.noreply.github.com>
2022-04-20 15:26:23 -0300

    Add files via upload

--- xatspace-lila/jason_list.js
+++ xatspace-lila/jason_list.js
@@ -1,100 +1,51 @@
 
 var listModel = [
       {
-            img: 'https://img.xatblog.net/image/SLVgnMLAd.png',
-            desc: 'Fundo  1'
+            img: 'https://i.postimg.cc/FHPxBzwb/inner-radio0120-bg.jpg',
+            desc: 'Fundo Interno - Cafafo'
       },
       {
-            img: 'https://img.xatblog.net/image/dDGJJpOtL.png',
-            desc: 'Fundo  2'
+            img: 'https://i.postimg.cc/69jHHbXs/radio0120-bg.jpg',
+            desc: 'Fundo Externo - Carinho e Amizade'
       },
       {
-            img: 'https://img.xatblog.net/image/TwlG5ef128G.png',
-            desc: 'Fundo   3 '
+            img: 'https://i.postimg.cc/ncmtghfD/inner-hola-01.jpg',
+            desc: 'Fundo Interno - Hola'
       },
       {
-            img: 'https://img.xatblog.net/image/ER9yanHHL7.png',
-            desc: 'Fundo 4'
+            img: 'https://i.postimg.cc/HkCCg1TP/hola-bg-01.jpg',
+            desc: 'Fundo Externo - Hola'
       },
       {
-            img: 'https://img.xatblog.net/image/P4l8P6wY8.png',
-            desc: 'Fundo 5'
+            img: 'https://i.postimg.cc/HkCCg1TP/hola-bg-01.jpg',
+            desc: 'Fundo Externo - Monicomio'
       },
       {
-            img: 'https://img.xatblog.net/image/PzU8J7W7L.png',
-            desc: 'Fundo 6'
+            img: 'https://i.postimg.cc/HkCCg1TP/hola-bg-01.jpg',
+            desc: 'Fundo Externo - Monicomio'
       },
       {
-            img: 'https://img.xatblog.net/image/ixcvTioY2qx.png',
-            desc: 'Fundo 7'
+            img: 'https://i.postimg.cc/FHPxBzwb/inner-radio0120-bg.jpg',
+            desc: 'Fundo Interno - Cafafo'
       },
       {
-            img: 'https://img.xatblog.net/image/IWD5YDPYmcT.png',
-            desc: 'Fundo 8'
+            img: 'https://i.postimg.cc/69jHHbXs/radio0120-bg.jpg',
+            desc: 'Fundo Externo - Carinho e Amizade'
       },
       {
-            img: 'https://img.xatblog.net/image/QHmKV6hRD.png',
-            desc: 'Fundo 9'
+            img: 'https://i.postimg.cc/ncmtghfD/inner-hola-01.jpg',
+            desc: 'Fundo Interno - Hola'
       },
       {
-            img: 'https://img.xatblog.net/image/u98Gzhewfpg.png',
-            desc: 'Fundo 10'
+            img: 'https://i.postimg.cc/HkCCg1TP/hola-bg-01.jpg',
+            desc: 'Fundo Externo - Hola'
       },
       {
-            img: 'https://img.xatblog.net/image/tXGVlvPwst.png',
-            desc: 'Fundo 11'
+            img: 'https://i.postimg.cc/HkCCg1TP/hola-bg-01.jpg',
+            desc: 'Fundo Externo - Monicomio'
       },
       {
-            img: 'https://img.xatblog.net/image/wScjgBqYcz4.png',
-            desc: 'Fundo 12'
+            img: 'https://i.postimg.cc/HkCCg1TP/hola-bg-01.jpg',
+            desc: 'Fundo Externo - Monicomio'
       },
-      {
-            img: 'https://img.xatblog.net/image/4ms7OSarN.png',
-            desc: 'Fundo 13'
-      },
-      {
-            img: 'https://img.xatblog.net/image/M5afCO90j.png',
-            desc: 'Fundo 14'
-      },
-      {
-            img: 'https://img.xatblog.net/image/abr5ulVd6.png',
-            desc: 'Fundo 15'
-      },
-      {
-            img: 'https://img.xatblog.net/image/W9bJh6wCkRz.png',
-            desc: 'Fundo 16'
-      },
-      {
-            img: 'https://img.xatblog.net/image/Br5ir9sUW1.png',
-            desc: 'Fundo 17'
-      },
-      {
-           img: 'https://img.xatblog.net/image/fkrAAqSGIIw.png',
-           desc: 'Fundo 18'
-      },
-      {
-           img: 'https://img.xatblog.net/image/sTQodSqwc.png',
-           desc: 'Fundo 19'
-      },
-      {
-           img: 'https://img.xatblog.net/image/bFJYoF3Ze.png',
-           desc: 'Fundo 20'
-      },
-      {
-           img: 'https://img.xatblog.net/image/33rGVn9EOfO.png',
-           desc: 'Fundo 21'
-      },
-      {
-           img: 'https://img.xatblog.net/image/sWGZxaxn2.png',
-           desc: 'Fundo 22'
-      },
-      
-   
-      
-      
-
-
-      
-      
-      
-      
+]
