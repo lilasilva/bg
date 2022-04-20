006b2ec2205bda907e191d397ea26043bdb5b11c
Lila Love <94986224+lilasilva@users.noreply.github.com>
2022-04-20 16:58:03 -0300

    Add files via upload

---
+++ jason_list1.js
@@ -0,0 +1,51 @@
+
+var listModel = [
+      {
+            img: 'https://i.imgur.com/cDXQlEz.png',
+            desc: 'Fundo Interno - 1'
+      },
+      {
+            img: 'https://i.imgur.com/BXjwbSl.png',
+            desc: 'Fundo Externo - 2'
+      },
+      {
+            img: 'https://i.imgur.com/SWDJ3Og.png',
+            desc: 'Fundo Interno - 3'
+      },
+      {
+            img: 'https://i.imgur.com/nnfSavh.png',
+            desc: 'Fundo Externo - 4'
+      },
+      {
+            img: 'https://i.imgur.com/wDY0nhg.png',
+            desc: 'Fundo Externo - 5'
+      },
+      {
+            img: 'https://i.imgur.com/lzG62qf.png',
+            desc: 'Fundo Externo - 6'
+      },
+      {
+            img: 'https://i.imgur.com/bcmxaUx.png',
+            desc: 'Fundo Interno - 7'
+      },
+      {
+            img: 'https://i.imgur.com/Bi4fIFs.png',
+            desc: 'Fundo Externo - 8'
+      },
+      {
+            img: 'https://i.imgur.com/o78s5EO.png',
+            desc: 'Fundo Interno - 9'
+      },
+      {
+            img: 'https://i.imgur.com/yudqknX.png',
+            desc: 'Fundo Externo - 10'
+      },
+      {
+            img: 'https://i.imgur.com/2sDbeRG.png',
+            desc: 'Fundo Externo - 11'
+      },
+      {
+            img: 'https://i.imgur.com/dWuiEv4.png',
+            desc: 'Fundo Externo - 12'
+      },
+]
