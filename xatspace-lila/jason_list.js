ce457bc9c9774dc7ef779ccb93f81b6a5e98d887
Lila Love <94986224+lilasilva@users.noreply.github.com>
2022-04-20 16:14:34 -0300

    Add files via upload

--- xatspace-lila/jason_list.js
+++ xatspace-lila/jason_list.js
@@ -1,51 +1,51 @@
 
 var listModel = [
       {
-            img: 'https://i.postimg.cc/FHPxBzwb/inner-radio0120-bg.jpg',
-            desc: 'Fundo Interno - Cafafo'
+            img: 'https://i.imgur.com/cDXQlEz.png',
+            desc: 'Fundo Interno - 1'
       },
       {
-            img: 'https://i.postimg.cc/69jHHbXs/radio0120-bg.jpg',
-            desc: 'Fundo Externo - Carinho e Amizade'
+            img: 'https://i.imgur.com/BXjwbSl.png',
+            desc: 'Fundo Externo - 2'
       },
       {
-            img: 'https://i.postimg.cc/ncmtghfD/inner-hola-01.jpg',
-            desc: 'Fundo Interno - Hola'
+            img: 'https://i.imgur.com/SWDJ3Og.png',
+            desc: 'Fundo Interno - 3'
       },
       {
-            img: 'https://i.postimg.cc/HkCCg1TP/hola-bg-01.jpg',
-            desc: 'Fundo Externo - Hola'
+            img: 'https://i.imgur.com/nnfSavh.png',
+            desc: 'Fundo Externo - 4'
       },
       {
-            img: 'https://i.postimg.cc/HkCCg1TP/hola-bg-01.jpg',
-            desc: 'Fundo Externo - Monicomio'
+            img: 'https://i.imgur.com/wDY0nhg.png',
+            desc: 'Fundo Externo - 5'
       },
       {
-            img: 'https://i.postimg.cc/HkCCg1TP/hola-bg-01.jpg',
-            desc: 'Fundo Externo - Monicomio'
+            img: 'https://i.imgur.com/lzG62qf.png',
+            desc: 'Fundo Externo - 6'
       },
       {
-            img: 'https://i.postimg.cc/FHPxBzwb/inner-radio0120-bg.jpg',
-            desc: 'Fundo Interno - Cafafo'
+            img: 'https://i.imgur.com/bcmxaUx.png',
+            desc: 'Fundo Interno - 7'
       },
       {
-            img: 'https://i.postimg.cc/69jHHbXs/radio0120-bg.jpg',
-            desc: 'Fundo Externo - Carinho e Amizade'
+            img: 'https://i.imgur.com/Bi4fIFs.png',
+            desc: 'Fundo Externo - 8'
       },
       {
-            img: 'https://i.postimg.cc/ncmtghfD/inner-hola-01.jpg',
-            desc: 'Fundo Interno - Hola'
+            img: 'https://i.imgur.com/o78s5EO.png',
+            desc: 'Fundo Interno - 9'
       },
       {
-            img: 'https://i.postimg.cc/HkCCg1TP/hola-bg-01.jpg',
-            desc: 'Fundo Externo - Hola'
+            img: 'https://i.imgur.com/yudqknX.png',
+            desc: 'Fundo Externo - 10'
       },
       {
-            img: 'https://i.postimg.cc/HkCCg1TP/hola-bg-01.jpg',
-            desc: 'Fundo Externo - Monicomio'
+            img: 'https://i.imgur.com/2sDbeRG.png',
+            desc: 'Fundo Externo - 11'
       },
       {
-            img: 'https://i.postimg.cc/HkCCg1TP/hola-bg-01.jpg',
-            desc: 'Fundo Externo - Monicomio'
+            img: 'https://i.imgur.com/dWuiEv4.png',
+            desc: 'Fundo Externo - 12'
       },
 ]
